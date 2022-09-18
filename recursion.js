0ceee4ee7917d13e8dd6e7eac35654274a51485d
Brian Elizondo <brian_elizondo@msn.com>
2022-09-17 21:29:28 -0400

    Data Structures => Recursion Exercises: Reverse String, Return a copy of a string, reversed

--- recursion.js
+++ recursion.js
@@ -38,9 +38,10 @@ function findIndex(arr, val, idx = 0){
 }
 
 /** revString: return a copy of a string, but in reverse. */
-
-function revString(str) {
-
+function revString(str, idx = 0, reverseStr = "") {
+    if(idx >= str.length) return reverseStr;
+    reverseStr = str[idx] + reverseStr;
+    return revString(str, idx + 1, reverseStr);
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
